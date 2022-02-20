a2b14723cb591ba60a20d0b7a6af01400d7e487e
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2022-02-20 22:28:46 +0100

    Updated AppHeroTag class.

--- lib/constant/app_hero_tag.dart
+++ lib/constant/app_hero_tag.dart
@@ -2,6 +2,6 @@ class AppHeroTag {
   static const appLogo = 'AppLogoAnimation';
   static const loginButton = 'LoginButtonAnimation';
   static const registerButton = 'RegisterButtonAnimation';
-  static const splitWidget = 'splitWidgetAnimation';
-  static const profileImage = 'profileImageAnimation';
+  static const splitWidget = 'SplitWidgetAnimation';
+  static const profileImage = 'ProfileImageAnimation';
 }
